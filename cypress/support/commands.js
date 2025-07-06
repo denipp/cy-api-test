71f44d0315f07aa72f0f2290739b4e84f0eafd1f
denipratama1 <deni.pratama@aux.mekari.com>
2025-07-06 18:58:23 +0700

    update file

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -27,10 +27,9 @@
 Cypress.Commands.add('apiRequest', (method, endpoint, options = {}) => {
     cy.request({
         method,
-        url: `${Cypress.env('baseUrl') || 'https://gorest.co.in/public/v2'}${endpoint}`, failOnStatusCode: false,
+        url: `${Cypress.env('baseUrl') || 'https://reqres.in/api'}${endpoint}`, failOnStatusCode: false,
         headers: {
             'x-api-key': 'reqres-free-v1',
-            'Authorization': `Bearer c6e1938c9268f231e7209ec36744768f87dd1fe33c389a2f28615a2a9166750c`,
             ...options.headers,
         },
         body: options.body,
